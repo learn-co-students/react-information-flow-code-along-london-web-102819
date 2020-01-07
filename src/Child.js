05ee8fa05b994054a7f79526b6e30e8311d99183
PianoFF <Fangfei1star@gmail.com>
2020-01-07 14:54:06 +0000

    Done.

--- src/Child.js
+++ src/Child.js
@@ -3,10 +3,13 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    // console.log(this.props); 
+    const {color, handleChangeColor} = this.props
     return (
       <div
+        onClick={() => handleChangeColor(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: color}}
       ></div>
     )
   }
